d9f6ca4390e20eb53c0405bf54264ac00d9527ce
Osirif <103280479+Osirif@users.noreply.github.com>
2024-08-09 13:58:20 +0500

    Daf

--- js/helloScreen.js
+++ js/helloScreen.js
@@ -1,7 +1,28 @@
-setTimeout(function () {
-  document.getElementById("header").style.opacity = "0";
-  setTimeout(function () {
-    document.getElementById("header").classList.add("hidden");
-  }, 1000);
-}, 1000);
+window.addEventListener('load', function() {
+  const loader = document.getElementById("loader");
+  const header = document.getElementById("header");
+  const headerContent = document.getElementById("header__content");
 
+  setTimeout(function () {
+    loader.style.transition = 'opacity 1s';
+    loader.style.opacity = '0';
+    setTimeout(function () {
+      loader.style.display = 'none';
+    }, 1000);
+  }, 500);
+
+  setTimeout(function () {
+    headerContent.style.transition = 'opacity 1s';
+    headerContent.style.opacity = '0';
+    setTimeout(function () {
+      headerContent.style.opacity = '1';
+    }, 1000);
+  }, 1000);
+
+  setTimeout(function () {
+    header.style.opacity = "0";
+    setTimeout(function () {
+      header.classList.add("hidden");
+    }, 1000);
+  }, 4000);
+});
