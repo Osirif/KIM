cb230c81a8a9fe699a09094c46e15ede51b0ee32
Osirif <103280479+Osirif@users.noreply.github.com>
2024-08-14 19:58:54 +0500

    Finally

--- js/helloScreen.js
+++ js/helloScreen.js
@@ -1,34 +1,40 @@
-window.addEventListener('load', function() {
-  const loader = document.getElementById("loader");
-  const header = document.getElementById("header");
-  const headerContent = document.getElementById("header__content");
-  const card1 = document.getElementById('card1');
+if (localStorage.getItem('firstVisit') !== 'true') {
+  window.addEventListener('load', function () {
+    const loader = document.getElementById("loader");
+    const header = document.getElementById("header");
+    const headerContent = document.getElementById("header__content");
+    const card1 = document.getElementById('card1');
 
-  // Hide loader with fade out effect after 1 second
-  setTimeout(function () {
-    loader.style.transition = 'opacity 1s';
-    loader.style.opacity = '0';
     setTimeout(function () {
-      loader.style.display = 'none';
+      loader.style.transition = 'opacity 1s';
+      loader.style.opacity = '0';
+      setTimeout(function () {
+        loader.style.display = 'none';
+      }, 1000);
     }, 1000);
-  }, 1000);
 
-  // Show header content with fade in effect after loader is hidden
-  setTimeout(function () {
-    headerContent.style.transition = 'opacity 1s';
-    headerContent.style.opacity = '0';
     setTimeout(function () {
-      headerContent.style.opacity = '1';
-    }, 1000);
-  }, 2000);
+      headerContent.style.transition = 'opacity 1s';
+      headerContent.style.opacity = '0';
+      setTimeout(function () {
+        headerContent.style.opacity = '1';
+      }, 1000);
+    }, 2000);
 
-  // Hide header after 2 seconds
-  setTimeout(function () {
-    header.style.opacity = "0";
     setTimeout(function () {
-      header.classList.add("hidden");
-      // Execute the function after header is hidden
-      flipCard(card1);
-    }, 1500);
-  }, 4000);
-});
+      header.style.opacity = "0";
+      setTimeout(function () {
+        header.classList.add("hidden");
+        flipCard(card1);
+      }, 1500);
+    }, 4000);
+  });
+  localStorage.setItem('firstVisit', 'true');
+}
+
+else {
+  header.classList.add("hidden");
+  setTimeout(function () {
+    flipCard(card1);
+  }, 1500);
+}
